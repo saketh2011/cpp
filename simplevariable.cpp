65c428be50d12888b6266d7f891b3620b1324d5d
saketh2011 <97312196+saketh2011@users.noreply.github.com>
2022-06-19 18:50:30 -0400

    Update simplevariable.cpp

--- simplevariable.cpp
+++ simplevariable.cpp
@@ -11,15 +11,20 @@ double price = 9999.99;
 //char ( 1 character ) variable
 char status = 'N';
 //string variable used for 1 or more variable
+int make = "1989 "
 string model = "Honda"
 //Bool ( True or false )
 bool for_sale = true;
 //new Value Changed
 for sale = false; 
+//combining 
+
+string car = make + model
+  
 //Print These Stuff
 cout << "Year: " << year <<endl
 cout << "Price: " << price << endl
-cout << "Status: " << status << endl
+cout << "Status: " << car << endl
 cout <<"MoDEL: " << model << endl
 cout << "For Sale: "for_sale << endl
 return 0;  
