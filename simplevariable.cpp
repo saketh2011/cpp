7ab0538eda831d7090016e9567c98e9e913b59ed
saketh2011 <97312196+saketh2011@users.noreply.github.com>
2022-06-19 13:47:48 -0400

    Create simplevariable.cpp

---
+++ simplevariable.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+//int (integer) variable
+int year = 2020;
+cout << year;
+//double variable with decimals
+double price = 9999.99;  
+//char ( 1 character ) variable
+char status = 'N';
+//string variable used for 1 or more variable
+string model = "Honda"
+//Bool ( True or false )
+bool for_sale = true;
+//new Value Changed
+for sale = false; 
+//Print These Stuff
+cout << "Year: " << year <<endl
+cout << "Price: " << price << endl
+cout << "Status: " << status << endl
+cout <<"MoDEL: " << model << endl
+cout << "For Sale: "for_sale << endl
+return 0;  
+}
