defe921bac4a78e954e3c12840c618132a50bc71
saketh2011 <97312196+saketh2011@users.noreply.github.com>
2022-06-19 19:44:43 -0400

    Create dreamthinginput.cpp

---
+++ dreamthinginput.cpp
@@ -0,0 +1,17 @@
+#inclue iostream
+#include <string>
+using namespace std;
+int main()
+{
+  string like;
+  int how much i want;
+  double pricey dicey;
+ cout << "What do you like alot ";
+ getline(cin,like)
+ cout << "How much would you like of that";
+ cin << how much i like;
+ cout << "How much you wanna pay for that";
+ cin << pricey dicey;
+ int x = how much i like * pricey dicey
+ cout <<"This is how much you gotta pay: "<< x << endl;
+ 
