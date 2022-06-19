cb9b109fff395118b19f71865f69d1d0fc38244c
saketh2011 <97312196+saketh2011@users.noreply.github.com>
2022-06-19 19:21:46 -0400

    Update userinput.cpp

--- userinput.cpp
+++ userinput.cpp
@@ -1,1 +1,18 @@
-include <iostream>
+#include <iostream>
+#include <string>
+using namespace std;
+
+int main()
+{
+  //variable = name
+ string name;
+ //question
+ cout << "Please tell me your name: "
+ //To add spaces you do getline cin. if Not its cin >> name(or other variable);
+ getline (cin,name);
+ cout << "Welcome " << name
+ cout << endl;
+ return 0;
+  
+  
+}
